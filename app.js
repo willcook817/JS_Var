08a7579c837a3393d4df5d81b0bc0126a57f9f66
willcook817 <OIC2@OIC2s-MacBook-Air.local>
2016-05-09 10:24:05 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// var myHome = 'vgiuvuivui'
+// var num = 69;
+// var  deez = 'how are you doing??'
+
+// alert(myHome);
+// console.log(deez);
+// document.write(num)
+var gangs = ["BD","GDK","GD","GDK",];
+console.log(gangs[0])
+
+
+
+
+console.log(gangs.length) 
+for(var i = 0; i <= gangs.length - 1; i++) {
+	console.log(i + " " +  gangs[i]);
+}
